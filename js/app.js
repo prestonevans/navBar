5ad1e00b1c89628d7cf2557a65ff834b2d1ce82a
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-09-30 07:37:24 -0600

    first attempt

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
-//document.body.style.backgroundColor = "red";
-//test me
+document.querySelector('.lines').addEventListener('click', () => {
+   let hiddenNav = document.querySelector('.hidden');
+   hiddenNav.style.display === 'none' ? hiddenNav.style.display = 'block' : hiddenNav.style.display = 'none'
+})
